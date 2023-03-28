a2960b6ed592e3ca238cd868b98c0ee8b6b83120
hibeenovul <hibeenovul19@gmail.com>
2023-03-28 14:01:54 +0100

    print long

--- print_long_hex.c
+++ print_long_hex.c
@@ -1,42 +1,44 @@
-#include "main.h"
-
+#include "m.h"
 /**
- * print_int - prints an integer
+ * prinlhex - prints a long decimal in hexadecimal
  * @arguments: input string
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
- * Return: number of chars printed.
+ * Return: number of chars printed
  */
-int print_int(va_list arguments, char *buf, unsigned int ibuf)
+int prinlhex(va_list arguments, char *buf, unsigned int ibuf)
 {
-	int int_input;
-	unsigned int int_in, int_temp, i, div, isneg;
+	long int int_input, i, isnegative, count, first_digit;
+	char *hexadecimal, *binary;
 
-	int_input = va_arg(arguments, int);
-	isneg = 0;
+	int_input = va_arg(arguments, long int);
+	isnegative = 0;
+	if (int_input == 0)
+	{
+		ibuf = handl_buf(buf, '0', ibuf);
+		return (1);
+	}
 	if (int_input < 0)
 	{
-		int_in = int_input * -1;
-		ibuf = handl_buf(buf, '-', ibuf);
-		isneg = 1;
-	}
-	else
-	{
-		int_in = int_input;
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
 	}
 
-	int_temp = int_in;
-	div = 1;
-
-	while (int_temp > 9)
+	binary = malloc(sizeof(char) * (64 + 1));
+	binary = fill_binary_array(binary, int_input, isnegative, 64);
+	hexadecimal = malloc(sizeof(char) * (16 + 1));
+	hexadecimal = fill_hex_array(binary, hexadecimal, 0, 16);
+	for (first_digit = i = count = 0; hexadecimal[i]; i++)
 	{
-		div *= 10;
-		int_temp /= 10;
+		if (hexadecimal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
+			count++;
+		}
 	}
-
-	for (i = 0; div > 0; div /= 10, i++)
-	{
-		ibuf = handl_buf(buf, ((int_in / div) % 10) + '0', ibuf);
-	}
-	return (i + isneg);
+	free(binary);
+	free(hexadecimal);
+	return (count);
 }
